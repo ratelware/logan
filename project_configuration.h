a1a84cb94496fbdc4630206ec9d2ff6e877235af
Tomasz Lewowski <tomasz.lewowski@wp.eu>
2020-08-17 20:52:21 +0200

    Initial configuration stuff

---
+++ project_configuration.h
@@ -0,0 +1,11 @@
+#ifndef PROJECT_CONFIGURATION_H
+#define PROJECT_CONFIGURATION_H
+
+
+class project_configuration
+{
+public:
+    project_configuration();
+};
+
+#endif // PROJECT_CONFIGURATION_H
---
+++ project_controller.cpp
@@ -0,0 +1,6 @@
+#include "project_controller.h"
+
+project_controller::project_controller()
+{
+
+}
