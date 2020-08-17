7d4dbd199abd7680ea7b8daa1a15e1f929cb91e6
Tomasz Lewowski <tomasz.lewowski@wp.eu>
2020-08-17 20:45:33 +0200

    Initial commit

---
+++ logan-cmake_en_US.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="logan-cmake_en_US">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="form.ui" line="16"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
